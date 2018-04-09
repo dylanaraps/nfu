4a67d2fdaa46d055dc927e3f4db2ceb3ad489da9
Dylan Araps <dylan.araps@gmail.com>
2018-04-09 11:59:15 +1000

    initial commit

--- uptime.c
+++ uptime.c
@@ -5,22 +5,16 @@ int main() {
     struct sysinfo info;
     int d, h, m;
 
-    if (sysinfo(&info) != 0)
-        return 1;
+    if (sysinfo(&info) != 0) return 1;
 
     d = info.uptime / 60 / 60 / 24;
     h = info.uptime / 60 / 60 % 24;
     m = info.uptime / 60 % 60;
 
     // Only print fields if they aren't '0'.
-    if (d != 0)
-        printf("%d%s ", d, "d");
-
-    if (h != 0)
-        printf("%d%s ", h, "h");
-
-    if (m != 0)
-        printf("%d%s", m, "m");
+    if (d^0) printf("%dd ", d);
+    if (h^0) printf("%dh ", h);
+    if (m^0) printf("%dm ", m);
 
     return 0;
 }
