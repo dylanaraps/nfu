ffc789ab26086f9ee8b744ef90a8c66e775a217b
Dylan Araps <dylan.araps@gmail.com>
2018-04-15 09:01:29 +1000

    general: Added resolution.

---
+++ resolution.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <X11/Xlib.h>
+
+int main() {
+    Display* d = XOpenDisplay(NULL);
+    Screen*  s = DefaultScreenOfDisplay(d);
+
+    printf("%dx%d \n", s->width, s->height);
+    return 0;
+}
