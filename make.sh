6da8fa00f4487471328ae12d4213827fda8640a2
Dylan Araps <dylan.araps@gmail.com>
2018-04-16 08:02:47 +1000

    general: Cleanup

--- make.sh
+++ make.sh
@@ -3,9 +3,10 @@
 # Simple build script.
 mkdir -p build
 
-for file in *.c; do
-    gcc -Wall -Wextra "$file" -o "build/${file%%.c}" 2>/dev/null || \
-    gcc -Wall -Wextra -lX11 -lXrandr "$file" -o "build/${file%%.c}"
+for file in src/*.c; do
+    file2="${file##*/}"
+    gcc -Wall -Wextra "$file" -o "build/${file2%%.c}" 2>/dev/null || \
+    gcc -Wall -Wextra -lX11 -lXrandr "$file" -o "build/${file2%%.c}"
 done
 
 printf "%s\\n" "Binaries are in ./build/"
