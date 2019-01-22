d9688da3e9261903b6a065edb12849ae1075e812
Dylan Araps <dylan.araps@gmail.com>
2019-01-22 18:06:44 +0200

    docs: update

--- src/resolution.c
+++ src/resolution.c
@@ -5,6 +5,6 @@ int main() {
     Display* d = XOpenDisplay(NULL);
     Screen*  s = DefaultScreenOfDisplay(d);
 
-    printf("%dx%d \n", s->width, s->height);
+    printf("%dx%d\n", s->width, s->height);
     return 0;
 }
